8d86a9577c37293ec3ce3298293e4fdbb4907421
AHMED HUSSAIN SAMBO <alhajimalamsambo442@gmail.com>
2024-11-09 11:39:35 +0000

    Update CardList.jsx

--- src/components/activityList/CardList.jsx
+++ src/components/activityList/CardList.jsx
@@ -6,7 +6,7 @@ import Card1 from "../activity/Card";
 
 const getData = async (page, cat) => {
   const res = await fetch(
-    `https://blog-one-blue-53.vercel.app/api/activitys?page=${page}&cat=${
+    `https://ugahsa.vercel.app/api/activitys?page=${page}&cat=${
       cat || ""
     }`,
     {
