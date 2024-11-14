e41e14e424b1cd58dfec1cc2fe067955e53058f6
AHMED HUSSAIN <alhajimalamsambo442@gmail.com>
2024-11-14 22:12:59 +0000

    new chnages

--- src/components/authLinks/AuthLinks.jsx
+++ src/components/authLinks/AuthLinks.jsx
@@ -1,4 +1,5 @@
 "use client";
+
 import Link from "next/link";
 import styles from "./authLinks.module.css";
 import { useState } from "react";
@@ -6,8 +7,9 @@ import { signOut, useSession } from "next-auth/react";
 
 const AuthLinks = () => {
   const [open, setOpen] = useState(false);
-
   const { status } = useSession();
+
+  const closeMenu = () => setOpen(false);
 
   return (
     <>
@@ -20,29 +22,54 @@ const AuthLinks = () => {
           <Link href="/write" className={styles.link}>
             Write
           </Link>
-          <span className={styles.link} onClick={signOut}>
+          <span
+            className={styles.link}
+            onClick={() => {
+              signOut();
+              closeMenu();
+            }}
+          >
             Logout
           </span>
         </>
       )}
+
       <div className={styles.burger} onClick={() => setOpen(!open)}>
         <div className={styles.line}></div>
         <div className={styles.line}></div>
         <div className={styles.line}></div>
       </div>
+
       {open && (
         <div className={styles.responsiveMenu}>
-          <Link href="/">Homepage</Link>
-          <Link href="/about">About</Link>
-          <Link href="/activities">Activities</Link>
+          <Link href="/" onClick={closeMenu}>
+            Homepage
+          </Link>
+          <Link href="/about" onClick={closeMenu}>
+            About
+          </Link>
+          <Link href="/activities" onClick={closeMenu}>
+            Activities
+          </Link>
 
-          {/* <Link href="/">Contact</Link> */}
-          {status === "notauthenticated" ? (
-            <Link href="/login">Login</Link>
+          {status === "unauthenticated" ? (
+            <Link href="/login" onClick={closeMenu}>
+              Login
+            </Link>
           ) : (
             <>
-              <Link href="/write">Write</Link>
-              <span className={styles.link}>Logout</span>
+              <Link href="/write" onClick={closeMenu}>
+                Write
+              </Link>
+              <span
+                className={styles.link}
+                onClick={() => {
+                  signOut();
+                  closeMenu();
+                }}
+              >
+                Logout
+              </span>
             </>
           )}
         </div>
